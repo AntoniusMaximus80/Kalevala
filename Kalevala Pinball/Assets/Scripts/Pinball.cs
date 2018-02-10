657dbe723c3eeac54a2e17773693ca42032a670d
Lauri Kosonen <lauri.kosonen@cs.tamk.fi>
2018-02-10 02:18:56 +0200

    Started working on ramp functionality. The ball's physics can now be toggled on/off and it can be stopped and launched by any object, including a ramp start.

---
+++ Kalevala Pinball/Assets/Scripts/Pinball.cs
@@ -0,0 +1,244 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Kalevala
+{
+    public class Pinball : MonoBehaviour
+    {
+        public bool debug_addImpulseForce;
+        public bool debug_stopMotion;
+        public bool debug_exitRamp;
+        //public bool debug_stopPhysics;
+        //public bool debug_startPhysics;
+
+        public Vector3 debug_upTableVelocity;
+        public float debug_vectorComparisonTolerance = 100;
+
+        public float speed;
+        public Vector3 rampVelocity;
+        public Vector3 oldVelocity;
+
+        private float radius;
+        private bool physicsEnabled = true;
+
+        private Ramp ramp;
+
+        private Rigidbody rb;
+        private SphereCollider sphColl;
+
+        private void Start()
+        {
+            radius = GetComponent<Collider>().bounds.size.x / 2;
+            rb = GetComponent<Rigidbody>();
+            sphColl = GetComponent<SphereCollider>();
+
+            debug_upTableVelocity = new Vector3(0f, 10 * 0.1742402f, 10 * -10.31068f);
+        }
+
+        private void Update()
+        {
+            speed = Speed;
+
+            if (physicsEnabled)
+            {
+                oldVelocity = PhysicsVelocity;
+            }
+
+            if (debug_stopMotion)
+            {
+                debug_stopMotion = false;
+                StopMotion();
+            }
+
+            if (debug_exitRamp)
+            {
+                debug_exitRamp = false;
+                if (IsOnRamp)
+                {
+                    ExitRamp();
+                }
+            }
+
+            //if (debug_stopPhysics)
+            //{
+            //    debug_stopPhysics = false;
+            //    SetPhysicsEnabled(false);
+            //}
+
+            //if (debug_startPhysics)
+            //{
+            //    debug_startPhysics = false;
+            //    SetPhysicsEnabled(true);
+            //}
+
+            if (debug_addImpulseForce)
+            {
+                debug_addImpulseForce = false;
+                AddImpulseForce(debug_upTableVelocity);
+            }
+        }
+
+        public float Speed
+        {
+            get
+            {
+                if (physicsEnabled)
+                {
+                    return rb.velocity.magnitude;
+                }
+                else
+                {
+                    return rampVelocity.magnitude;
+                }
+            }
+        }
+
+        public Vector3 PhysicsVelocity
+        {
+            get
+            {
+                return rb.velocity;
+            }
+        }
+
+        public void StopMotion()
+        {
+            if (physicsEnabled)
+            {
+                rb.velocity = Vector3.zero;
+                rb.angularVelocity = Vector3.zero;
+            }
+        }
+
+        public void SetPhysicsEnabled(bool enable)
+        {
+            if (enable)
+            {
+                if (!physicsEnabled)
+                {
+                    physicsEnabled = true;
+                    rb.isKinematic = false;
+                    rb.useGravity = true;
+                    sphColl.enabled = true;
+                }
+            }
+            else if (physicsEnabled)
+            {
+                StopMotion();
+                physicsEnabled = false;
+                rb.isKinematic = true;
+                rb.useGravity = false;
+                sphColl.enabled = false;
+            }
+        }
+
+        public void AddImpulseForce(Vector3 force)
+        {
+            if (physicsEnabled)
+            {
+                rb.AddForce(force, ForceMode.Impulse);
+
+                //Vector3 forcePosition = transform.position;
+                //forcePosition.y += radius * 3f / 4f;
+                //rb.AddForceAtPosition(force, forcePosition, ForceMode.Impulse);
+            }
+        }
+
+        public bool IsOnRamp
+        {
+            get
+            {
+                return ramp != null;
+            }
+        }
+
+        public void EnterRamp(Ramp ramp, Vector3 debug_rampDirection)
+        {
+            Debug.Log("Ramp entered");
+            float speedEnteringRamp = Speed;
+            this.ramp = ramp;
+            SetPhysicsEnabled(false);
+
+            // TODO: Moving on a rail
+            // Direction from line, speed from speedEnteringRamp
+
+            // Testing
+            rampVelocity = debug_rampDirection * speedEnteringRamp;
+            ExitRamp();
+        }
+
+        public void ExitRamp()
+        {
+            Debug.Log("Ramp exited");
+            float speedExitingRamp = Speed;
+            ramp = null;
+            SetPhysicsEnabled(true);
+
+            // TODO: Getting the right direction for impulse
+
+            // Testing
+            AddImpulseForce(rampVelocity);
+
+            rampVelocity = Vector3.zero;
+        }
+
+        //public bool SameDirections(Vector3 direction1, Vector3 direction2, float angleTolerance)
+        //{
+        //    bool result = false;
+
+        //    if (Vector3.Angle(direction1, direction2) <= angleTolerance)
+        //    {
+        //        result = true;
+        //    }
+
+        //    return result;
+        //}
+
+        private void OnDrawGizmos()
+        {
+            DrawDirection();
+            //DrawDirection2();
+            //DrawLocalAxes();
+
+            if (IsOnRamp)
+            {
+                DrawColoredWireSphere();
+            }
+        }
+
+        private void DrawDirection()
+        {
+            if (Application.isPlaying)
+            {
+                //if (SameDirections(PhysicsVelocity, debug_directionVector, debug_vectorComparisonTolerance))
+                //{
+                //    Gizmos.color = Color.green;
+                //}
+                //else
+                //{
+                //    Gizmos.color = Color.red;
+                //}
+
+                Gizmos.color = Color.red;
+                Gizmos.DrawLine(transform.position, transform.position + PhysicsVelocity.normalized * radius * 10);
+            }
+        }
+
+        private void DrawLocalAxes()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(transform.position, transform.position + transform.right * radius * 3);
+            Gizmos.color = Color.green;
+            Gizmos.DrawLine(transform.position, transform.position + transform.up * radius * 3);
+            Gizmos.color = Color.blue;
+            Gizmos.DrawLine(transform.position, transform.position + transform.forward * radius * 3);
+        }
+
+        private void DrawColoredWireSphere()
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawWireSphere(transform.position, radius * 1.1f);
+        }
+    }
+}
---
+++ Kalevala Pinball/Assets/Scripts/RampStart.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Kalevala
+{
+    public class RampStart : MonoBehaviour
+    {
+        public Vector3 startPoint;
+        public Vector3 rampDirection;
+        public float distanceTolerance = 0.2f;
+        public float directionAngleTolerance = 30;
+
+        public List<Pinball> pinballs;
+        public Ramp ramp;
+
+        /* The ramp start knows the ramp it is a part of
+         * When a pinball hits, the ramp is given to it and it disables its physics
+         * The pinball moves itself on the ramp using its speed, the ramp's line and gravity
+         * The pinball either changes direction midway or reaches the end of the ramp
+         * When the pinball should exit the ramp, it forgets the ramp, enables its physics
+           and adds force to itself based on the speed it had on the ramp
+         */
+
+        private void Start()
+        {
+            rampDirection.Normalize();
+        }
+
+        private void Update()
+        {
+            foreach (Pinball ball in pinballs)
+            {
+                if (!ball.IsOnRamp)
+                {
+                    if (Hit(ball.transform.position))
+                    {
+                        if (SameDirections(ball.PhysicsVelocity))
+                        {
+                            ball.EnterRamp(ramp, rampDirection);
+                        }
+                    }
+                }
+            }
+        }
+
+        public bool Hit(Vector3 objPosition)
+        {
+            bool result = false;
+
+            if (Vector3.Distance(transform.position + startPoint, objPosition) < distanceTolerance)
+            {
+                result = true;
+            }
+
+            return result;
+        }
+
+        public bool SameDirections(Vector3 ballDirection)
+        {
+            bool result = false;
+
+            if (Vector3.Angle(ballDirection, rampDirection) <= directionAngleTolerance)
+            {
+                result = true;
+            }
+
+            return result;
+        }
+
+        private void OnDrawGizmos()
+        {
+            DrawStartPoint();
+            DrawDirection();
+        }
+
+        private void DrawStartPoint()
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawWireSphere(transform.position + startPoint, 0.8f);
+        }
+
+        private void DrawDirection()
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawLine(transform.position + startPoint, transform.position + startPoint + rampDirection * 8);
+        }
+    }
+}
