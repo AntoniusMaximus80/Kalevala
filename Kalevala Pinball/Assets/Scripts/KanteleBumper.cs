125bb7de4d0e8b08a2ccb33053d788b7a31f83fc
AntoniusMaximus80 <toni.mutanen@cs.tamk.fi>
2018-02-10 17:37:07 +0200

    Working kantele bumpers. They still need better animations.

---
+++ Kalevala Pinball/Assets/Scripts/KanteleBumper.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+namespace Kalevala
+{
+    public class KanteleBumper : MonoBehaviour
+    {
+        public GameObject _bumperInactive,
+            _bumperActive,
+            _bumperForceOrigin;
+        public Scorekeeper _scoreKeeper;
+        public int _bumperActiveFrames,
+            _bumperScore;
+        private int _bumperActiveCountdown = 0;
+        public float _bumperForce;
+        private Collider _bumperCollider;
+
+        // Use this for initialization
+        void Start()
+        {
+            _bumperCollider = GetComponent<Collider>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (_bumperActiveCountdown != 0)
+            {
+                _bumperActiveCountdown--;
+                if (_bumperActiveCountdown == 0)
+                {
+                    _bumperCollider.enabled = true;
+                    _bumperActive.SetActive(false);
+                    _bumperInactive.SetActive(true);
+                }
+            }
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            _scoreKeeper.AddScore(_bumperScore);
+            _bumperActiveCountdown = _bumperActiveFrames;
+            _bumperCollider.enabled = false;
+            _bumperInactive.SetActive(false);
+            _bumperActive.SetActive(true);
+            other.GetComponent<Rigidbody>().AddForce(other.transform.position - _bumperForceOrigin.transform.position * _bumperForce, ForceMode.Impulse);
+        }
+    }
+}
