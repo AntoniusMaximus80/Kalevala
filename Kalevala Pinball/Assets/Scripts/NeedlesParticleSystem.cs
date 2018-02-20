22270de90a1bfc496c2db8764d1bc8a5e051c2b8
AntoniusMaximus80 <toni.mutanen@cs.tamk.fi>
2018-02-20 15:26:44 +0200

    Spruce Bumpers now instantiate particle systems on hit and play sounds.

---
+++ Kalevala Pinball/Assets/Scripts/NeedlesParticleSystem.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NeedlesParticleSystem : MonoBehaviour
+{
+    private float _particleSystemDuration,
+        _destroyCounter;
+
+    // Use this for initialization
+    void Start()
+    {
+        _particleSystemDuration = GetComponent<ParticleSystem>().main.duration;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _destroyCounter += Time.deltaTime;
+        if (_destroyCounter >= _particleSystemDuration)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Kalevala Pinball/Assets/Scripts/SpruceBumper.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Kalevala
+{
+    public class SpruceBumper : MonoBehaviour
+    {
+        public GameObject _needlesParticleSystemPrefab;
+        private AudioSource _spruceBumperHitAudioSource;
+
+        [SerializeField, Range(0f, 32f)]
+        private float _bumperForce;
+
+        void Start()
+        {
+            _spruceBumperHitAudioSource = GetComponent<AudioSource>();
+        }
+
+        void Update()
+        {
+
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            Debug.Log("Spruce Bumper collision!");
+            Vector3 difference = other.gameObject.transform.position - gameObject.transform.position;
+            difference.y = 0f;
+            other.gameObject.GetComponent<Rigidbody>().AddForce(difference * _bumperForce, ForceMode.Impulse);
+
+            Instantiate(_needlesParticleSystemPrefab, transform);
+
+            if (!_spruceBumperHitAudioSource.isPlaying)
+            {
+                float randomPitch = Random.Range(0.8f, 1.2f);
+                _spruceBumperHitAudioSource.pitch = randomPitch;
+                _spruceBumperHitAudioSource.Play();
+            }
+        }
+    }
+}
