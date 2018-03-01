99f533c0e1530fad361657d464854b3eda9deb8e
Valtteri Ojanen <valtteri.ojanen@cs.tamk.fi>
2018-03-01 16:17:24 +0200

    Skillshot initial

---
+++ Kalevala Pinball/Assets/Scripts/SkillShotHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Kalevala
+{
+    public class SkillShotHandler: MonoBehaviour
+    {
+
+        private void OnTriggerEnter( Collider other )
+        {
+            Pinball ball = other.GetComponent<Pinball>();
+            if(ball != null)
+            {
+                ball.transform.position = new Vector3(0, 1f, 0f);
+            }
+        }
+
+    }
+}
