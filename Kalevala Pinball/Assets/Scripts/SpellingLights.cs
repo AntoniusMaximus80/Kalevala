ad5611a91d3f975a1f50feaafd6760ed25686b66
AntoniusMaximus80 <toni.mutanen@cs.tamk.fi>
2018-03-10 21:19:03 +0200

    Minor fixes.

--- Kalevala Pinball/Assets/Scripts/SpellingLights.cs
+++ Kalevala Pinball/Assets/Scripts/SpellingLights.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CyclingLights : MonoBehaviour {
+public class SpellingLights : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
