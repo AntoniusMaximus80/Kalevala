5e6cbfd5da9ddc448175fbd118e5cd68f073c110
AntoniusMaximus80 <toni.mutanen@cs.tamk.fi>
2018-03-10 11:58:41 +0200

    Reworked pinball bottom elements.

---
+++ Kalevala Pinball/Assets/Scripts/LanguageChanger.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Kalevala
+{
+    public class LanguageChanger : MonoBehaviour
+    {
+        [SerializeField]
+        private string _textID;
+
+        private Text _text;
+
+        private void Start()
+        {
+            _text = GetComponent<Text>();
+            if (_text == null)
+            {
+                Debug.LogError("Text component not found in the object");
+            }
+            else
+            {
+                UpdateText();
+            }
+        }
+
+        public void UpdateText()
+        {
+            if (_text != null)
+            {
+                _text.text = GameManager.Instance.Language.GetText(_textID);
+            }
+        }
+    }
+}
