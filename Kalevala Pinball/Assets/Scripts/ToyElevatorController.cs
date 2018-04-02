eea7f17304485ab211fc8deb7549f40e644f45e3
AntoniusMaximus80 <toni.mutanen@cs.tamk.fi>
2018-04-02 18:12:12 +0300

    The toy elevator now rises and lowers through the iris door.

---
+++ Kalevala Pinball/Assets/Scripts/ToyElevatorController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Kalevala
+{
+    public class ToyElevatorController : MonoBehaviour
+    {
+        [SerializeField]
+        private Animator _toyElevatorAnimator,
+            _irisDoorAnimator;
+
+        private bool _elevatorUp = false;
+
+        private float _animationDuration = 4f,
+            _animationCounter = 0f;
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        _animationCounter += Time.deltaTime;
+            if (_animationCounter > _animationDuration) {
+                _animationCounter = 0f;
+                if (_elevatorUp)
+                {
+                    _elevatorUp = false;
+                    LowerElevator();
+                } else
+                {
+                    _elevatorUp = true;
+                    RaiseElevator();
+                }
+            }
+        }
+
+        void RaiseElevator()
+        {
+            _toyElevatorAnimator.SetBool("Rise", true);
+            _irisDoorAnimator.SetBool("Open", true);
+        }
+
+        void LowerElevator()
+        {
+            _toyElevatorAnimator.SetBool("Rise", false);
+            _irisDoorAnimator.SetBool("Open", false);
+        }
+    }
+}
