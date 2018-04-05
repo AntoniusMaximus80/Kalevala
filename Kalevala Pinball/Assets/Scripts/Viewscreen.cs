a88e4945eb36833b44909d4b243277bcea8ce374
VNiemi <ville.niemi@sykemediapaja.com>
2018-04-06 01:30:09 +0300

    Refactored old scorekeeping functionality.

--- Kalevala Pinball/Assets/Scripts/Viewscreen.cs
+++ Kalevala Pinball/Assets/Scripts/Viewscreen.cs
@@ -17,29 +17,44 @@ namespace Kalevala
             _smallScore,
             _gameMode;
 
-        private Viewscreen _instance;
+        public float _incrementVisible = 5f;
+
+        private static float _incrementVisibleCountdown;
+
+        private Color _fade = new Color(1f, 1f, 1f, 1f);
+
+        private static Viewscreen _instance;
 
         
         // Use this for initialization
         void Awake()
         {
             _instance = this;
+            _incrementVisibleCountdown = _incrementVisible;
         }
 
         // Update is called once per frame
         void Update()
         {
-
+            if (_incrementVisibleCountdown != 0f)
+            {
+                _incrementVisibleCountdown -= Time.deltaTime;
+                Mathf.Clamp01(_incrementVisibleCountdown);
+                float ratio = _incrementVisibleCountdown / _incrementVisible;
+                _fade.a = ratio;
+                _incrementUGUI.color = _fade;
+            }
         }
 
         public static void FormatScore(int score)
         {
-
+            _instance._scoreUGUI.text = score.ToString("N0");
         }
 
         public static void FormatScoreIncrement(int score)
         {
-
+            _instance._incrementUGUI.text = score.ToString("N0");
+            _incrementVisibleCountdown = _instance._incrementVisible;
         }
     }
 }
