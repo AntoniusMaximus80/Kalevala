da42914e8292d11a532e2c9235fcc45e2ee00910
VNiemi <ville.niemi@cs.tamk.fi>
2018-04-12 11:10:31 +0300

    Increased physics iterations.

--- Kalevala Pinball/Assets/Scripts/PhysObjects/Pinball.cs
+++ Kalevala Pinball/Assets/Scripts/PhysObjects/Pinball.cs
@@ -53,7 +53,7 @@ namespace Kalevala
             // Trying to improve physics by overriding the defaults for the ball.
             _rb.maxAngularVelocity = 0;
             _rb.maxDepenetrationVelocity = _rb.maxDepenetrationVelocity * 5;
-            _rb.solverIterations = 12;
+            _rb.solverIterations = 30;
             _rb.solverVelocityIterations = 5;
             _sphColl.contactOffset = 0.1f;
           
