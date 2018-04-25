54631966118ce77d13e6429ed834499f35cc5435
VNiemi <ville.niemi@sykemediapaja.com>
2018-04-25 19:41:34 +0300

    Initial save for strobe lights. Does nothing, but needed for backup. (And should be only commit changing the scene as it has all the linking.)

---
+++ Kalevala Pinball/Assets/Scripts/Managers/StrobeLight.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Kalevala {
+
+    public class StrobeLight : MonoBehaviour
+    {
+        public Renderer _cover;
+        public GameObject _lightBulb;
+        public Light _pointLight;
+
+        private bool _state;
+
+        private float _switchTime = -10f;
+
+
+        public void Switch(bool onoff)
+        {
+            _state = onoff;
+
+            //Up.enabled = onoff;
+            //Down.enabled = onoff;
+
+           
+
+            _switchTime = Time.time;
+        }
+
+        public void Update()
+        {
+
+            bool value;
+
+            if ((Time.time - _switchTime) < .5f)
+            {
+                int time = Mathf.RoundToInt((Time.time - _switchTime) * 100);
+
+                int cycle = time / 10;
+                int phase = time % 5;
+
+                value = (phase < cycle) ? _state : !_state;
+            }
+            else
+            {
+                value = _state;
+            }
+
+            //Up.enabled = value;
+            //Down.enabled = value;
+
+
+        }
+
+    }
+}
